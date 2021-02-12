d322e71116dca660bcc74ee30cbe75987838c87e
ringochuchudull <ringo.chu.16@ucl.ac.uk>
2021-02-13 01:00:46 +0800

    the stress is high

--- ours/dataloader/vidvrdloader.py
+++ ours/dataloader/vidvrdloader.py
@@ -1,3 +1,7 @@
+'''
+Modified from: https://github.com/RaivoKoot/Video-Dataset-Loading-Pytorch
+'''
+
 import os
 import glob
 import torch
@@ -13,6 +17,7 @@ class VideoVRDLoader(GeneralLoader):
         super(GeneralLoader, self).__init__()
         # load all image files, sorting them to
         # ensure that they are aligned
+
         self.video_names = []       # Name [name1, name2 ... name_n]
         self._videos_frames = []    # Video frames path [ [video1 frames1]... [vidoo frames n] ]
 
@@ -20,7 +25,9 @@ class VideoVRDLoader(GeneralLoader):
         self._classes = []
         self._classes_info = []
 
-        self.total_classes = []
+        self._relation_instace = []
+
+        #self.total_classes = []
 
         self.root = data_path
         assert os.path.exists(self.root), "Your -->  --data_path <-- got problem"
@@ -41,14 +48,11 @@ class VideoVRDLoader(GeneralLoader):
             # Only keep the frames with Boxes
             video_frame_path = video_frame_path[0:len(info['trajectories'])]
 
-            self.video_names.append(video_id)
-            self._videos_frames.append(video_frame_path)
-
             # One Video
             this_objid = {so["tid"]:so["category"] for so in info["subject/objects"]}
             _bb, _cls = [], []
             _rel = [[] for _ in range(len(info['trajectories']))]
-            
+
             for idx, traj in enumerate(info["trajectories"]):
 
                 if np.equal(len(traj), 0):
@@ -59,18 +63,17 @@ class VideoVRDLoader(GeneralLoader):
                     _bb.append([t["bbox"]["xmin"], t["bbox"]["ymin"], t["bbox"]["xmax"], t["bbox"]["ymax"]])
                     _cls.append(this_objid[t["tid"]])
 
+                for r in info["relation_instances"]:
+                    if r['begin_fid'] <= idx <= r['end_fid']:
+                        _rel[idx].append([r['subject_tid'], r['predicate'], r['object_tid']])
+
+            self.video_names.append(video_id)
+            self._videos_frames.append(video_frame_path)
             self._bbs_info.append(_bb)
             self._classes.append(_cls)
             self._classes_info.append(this_objid)
-
-            # Relations
-            print(f'Relation Instances:\n {info["relation_instances"]}')
-
-            _rel = [[] for _ in range(len(info['trajectories']))]
-            for ri in info["relation_instances"]:
-                start, end = ri["begin_fid"], end
-
-
+            self._relation_instace.append(_rel)
+            assert len(self._relation_instace) == len(self._bbs_info) == len(self._classes) == len(self._classes_info)
 
         self._vis_threshold = _vis_threshold
         self.transforms = transforms
@@ -154,3 +157,22 @@ class VideoVRDLoader(GeneralLoader):
 
     def __str__(self):
         return 'This is General Detection LOADER'
+
+
+class ImglistToTensor(torch.nn.Module):
+    """
+    Converts a list of PIL images in the range [0,255] to a torch.FloatTensor
+    of shape (NUM_IMAGES x CHANNELS x HEIGHT x WIDTH) in the range [0,1].
+    Can be used as first transform for ``VideoFrameDataset``.
+    """
+    def forward(self, img_list):
+        """
+        Converts each PIL image in a list to
+        a torch Tensor and stacks them into
+        a single tensor.
+        Args:
+            img_list: list of PIL images.
+        Returns:
+            tensor of size ``NUM_IMAGES x CHANNELS x HEIGHT x WIDTH``
+        """
+        return torch.stack([transforms.functional.to_tensor(pic) for pic in img_list])
